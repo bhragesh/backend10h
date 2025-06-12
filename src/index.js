3f3352a9e6ea5a52140d25c40c74b7bc3088596d
bhrageshpatel <bhrageshpatel@gmail.com>
2025-06-12 23:12:34 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// import mongoose from 'mongoose';
+// import {dabba} from "./constants";
+
+// require('dotenv').config({path:'./env'})
+// or
+import dotenv from "dotenv"
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path:'./env'
+})
